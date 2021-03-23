ca5aba4c1ed9377615fd1bfaaf421abe3da03b78
Benoit Ferretti <benoit.ferretti@edu.em-lyon.com>
2021-03-23 13:18:38 +0100

    navigation implemented

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import * as React from "react";
+import { View, Button, StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import LoginScreen from './screens/LoginScreen';
 
-export default function App() {
+function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button
+        title="Se connecter"
+        onPress={() => navigation.navigate('Se connecter')}
+      />
+      <Button
+        title="S'inscrire"
+        onPress={() => Alert.alert("Simple Button pressed")}
+      />
     </View>
   );
 }
 
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Accueil" component={HomeScreen} />
+        <Stack.Screen name="Se connecter" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+export default App;
---
+++ screens/LoginScreen.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { View, Button, Text } from "react-native";
+
+function LoginScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Page se connecter</Text>
+    </View>
+  );
+}
+
+export default LoginScreen;
